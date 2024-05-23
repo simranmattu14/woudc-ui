efab8e870dabe577d8c6b8e77e5e98596a1afa21
Mattu <Simran.Mattu@ec.gc.ca>
2024-05-23 15:11:07 -0400

    Added all the images with graph-carousel

--- pages/data/products/normal-ozone-maps.vue
+++ pages/data/products/normal-ozone-maps.vue
@@ -19,161 +19,22 @@
         :key="Object.keys(normalOzoneMapsGlobal)"
         :graphs="normalOzoneMapsGlobal"
       ></graph-carousel>
-      <!-- <v-row>
-        <v-col>
-          <img :src="`${baseOzoneURL}/gl_cl/to01.gif`" class="recent" />
-        </v-col>
-        <v-col>
-          <img :src="`${baseOzoneURL}/gl_cl/to02.gif`" class="recent" />
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <img :src="`${baseOzoneURL}/gl_cl/to03.gif`" class="recent" />
-        </v-col>
-        <v-col>
-          <img :src="`${baseOzoneURL}/gl_cl/to04.gif`" class="recent" />
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <img :src="`${baseOzoneURL}/gl_cl/to05.gif`" class="recent" />
-        </v-col>
-        <v-col>
-          <img :src="`${baseOzoneURL}/gl_cl/to06.gif`" class="recent" />
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <img :src="`${baseOzoneURL}/gl_cl/to07.gif`" class="recent" />
-        </v-col>
-        <v-col>
-          <img :src="`${baseOzoneURL}/gl_cl/to08.gif`" class="recent" />
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <img :src="`${baseOzoneURL}/gl_cl/to09.gif`" class="recent" />
-        </v-col>
-        <v-col>
-          <img :src="`${baseOzoneURL}/gl_cl/to10.gif`" class="recent" />
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <img :src="`${baseOzoneURL}/gl_cl/to11.gif`" class="recent" />
-        </v-col>
-        <v-col>
-          <img :src="`${baseOzoneURL}/gl_cl/to12.gif`" class="recent" />
-        </v-col>
-      </v-row> -->
     </section>
 
     <section v-if="ozoneMapTab === 'northern'">
       <h2>{{ $t('data.products.normal-ozone-maps.northernDesc') }}</h2>
-      <v-row>
-        <v-col>
-          <img :src="`${baseOzoneURL}/nh_cl/to01.gif`" class="recent" />
-        </v-col>
-        <v-col>
-          <img :src="`${baseOzoneURL}/nh_cl/to02.gif`" class="recent" />
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <img :src="`${baseOzoneURL}/nh_cl/to03.gif`" class="recent" />
-        </v-col>
-        <v-col>
-          <img :src="`${baseOzoneURL}/nh_cl/to04.gif`" class="recent" />
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <img :src="`${baseOzoneURL}/nh_cl/to05.gif`" class="recent" />
-        </v-col>
-        <v-col>
-          <img :src="`${baseOzoneURL}/nh_cl/to06.gif`" class="recent" />
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <img :src="`${baseOzoneURL}/nh_cl/to07.gif`" class="recent" />
-        </v-col>
-        <v-col>
-          <img :src="`${baseOzoneURL}/nh_cl/to08.gif`" class="recent" />
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <img :src="`${baseOzoneURL}/nh_cl/to09.gif`" class="recent" />
-        </v-col>
-        <v-col>
-          <img :src="`${baseOzoneURL}/nh_cl/to10.gif`" class="recent" />
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <img :src="`${baseOzoneURL}/nh_cl/to11.gif`" class="recent" />
-        </v-col>
-        <v-col>
-          <img :src="`${baseOzoneURL}/nh_cl/to12.gif`" class="recent" />
-        </v-col>
-      </v-row>
+      <graph-carousel
+        :key="Object.keys(normalOzoneMapsNorthern)"
+        :graphs="normalOzoneMapsNorthern"
+      ></graph-carousel>
     </section>
 
     <section v-if="ozoneMapTab === 'southern'">
       <h2>{{ $t('data.products.normal-ozone-maps.southernDesc') }}</h2>
-      <v-row>
-        <v-col>
-          <img :src="`${baseOzoneURL}/sh_cl/to01.gif`" class="recent" />
-        </v-col>
-        <v-col>
-          <img :src="`${baseOzoneURL}/sh_cl/to02.gif`" class="recent" />
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <img :src="`${baseOzoneURL}/sh_cl/to03.gif`" class="recent" />
-        </v-col>
-        <v-col>
-          <img :src="`${baseOzoneURL}/sh_cl/to04.gif`" class="recent" />
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <img :src="`${baseOzoneURL}/sh_cl/to05.gif`" class="recent" />
-        </v-col>
-        <v-col>
-          <img :src="`${baseOzoneURL}/sh_cl/to06.gif`" class="recent" />
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <img :src="`${baseOzoneURL}/sh_cl/to07.gif`" class="recent" />
-        </v-col>
-        <v-col>
-          <img :src="`${baseOzoneURL}/sh_cl/to08.gif`" class="recent" />
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <img :src="`${baseOzoneURL}/sh_cl/to09.gif`" class="recent" />
-        </v-col>
-        <v-col>
-          <img :src="`${baseOzoneURL}/sh_cl/to10.gif`" class="recent" />
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <img :src="`${baseOzoneURL}/sh_cl/to11.gif`" class="recent" />
-        </v-col>
-        <v-col>
-          <img
-            :src="`${baseOzoneClimatologyURL}/sh_cl/to12.gif`"
-            class="recent"
-          />
-        </v-col>
-      </v-row>
+      <graph-carousel
+        :key="Object.keys(normalOzoneMapsSouthern)"
+        :graphs="normalOzoneMapsSouthern"
+      ></graph-carousel>
     </section>
   </v-container>
 </template>
@@ -195,52 +56,266 @@ export default {
       const { baseOzoneClimatologyURL } = this
       return [
         {
-          caption: '',
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.JanDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.global'),
           url: `${baseOzoneClimatologyURL}/gl_cl/to01.gif`,
         },
         {
-          caption: '',
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.FebDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.global'),
           url: `${baseOzoneClimatologyURL}/gl_cl/to02.gif`,
         },
         {
-          caption: '',
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.MarDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.global'),
           url: `${baseOzoneClimatologyURL}/gl_cl/to03.gif`,
         },
         {
-          caption: '',
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.AprilDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.global'),
           url: `${baseOzoneClimatologyURL}/gl_cl/to04.gif`,
         },
         {
-          caption: '',
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.MayDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.global'),
           url: `${baseOzoneClimatologyURL}/gl_cl/to05.gif`,
         },
         {
-          caption: '',
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.JuneDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.global'),
           url: `${baseOzoneClimatologyURL}/gl_cl/to06.gif`,
         },
         {
-          caption: '',
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.JulyDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.global'),
           url: `${baseOzoneClimatologyURL}/gl_cl/to07.gif`,
         },
         {
-          caption: '',
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.AugustDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.global'),
           url: `${baseOzoneClimatologyURL}/gl_cl/to08.gif`,
         },
         {
-          caption: '',
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.SeptDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.global'),
           url: `${baseOzoneClimatologyURL}/gl_cl/to09.gif`,
         },
         {
-          caption: '',
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.OctDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.global'),
           url: `${baseOzoneClimatologyURL}/gl_cl/to10.gif`,
         },
         {
-          caption: '',
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.NovDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.global'),
           url: `${baseOzoneClimatologyURL}/gl_cl/to11.gif`,
         },
         {
-          caption: '',
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.DecDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.global'),
           url: `${baseOzoneClimatologyURL}/gl_cl/to12.gif`,
+        },
+      ]
+    },
+    normalOzoneMapsNorthern() {
+      const { baseOzoneClimatologyURL } = this
+      return [
+        {
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.JanDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.northern'),
+          url: `${baseOzoneClimatologyURL}/nh_cl/to01.gif`,
+        },
+        {
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.FebDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.northern'),
+          url: `${baseOzoneClimatologyURL}/nh_cl/to02.gif`,
+        },
+        {
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.MarDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.northern'),
+          url: `${baseOzoneClimatologyURL}/nh_cl/to03.gif`,
+        },
+        {
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.AprilDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.northern'),
+          url: `${baseOzoneClimatologyURL}/nh_cl/to04.gif`,
+        },
+        {
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.MayDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.northern'),
+          url: `${baseOzoneClimatologyURL}/nh_cl/to05.gif`,
+        },
+        {
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.JuneDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.northern'),
+          url: `${baseOzoneClimatologyURL}/nh_cl/to06.gif`,
+        },
+        {
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.JulyDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.northern'),
+          url: `${baseOzoneClimatologyURL}/nh_cl/to07.gif`,
+        },
+        {
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.AugustDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.northern'),
+          url: `${baseOzoneClimatologyURL}/nh_cl/to08.gif`,
+        },
+        {
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.SeptDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.northern'),
+          url: `${baseOzoneClimatologyURL}/nh_cl/to09.gif`,
+        },
+        {
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.OctDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.northern'),
+          url: `${baseOzoneClimatologyURL}/nh_cl/to10.gif`,
+        },
+        {
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.NovDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.northern'),
+          url: `${baseOzoneClimatologyURL}/nh_cl/to11.gif`,
+        },
+        {
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.DecDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.northern'),
+          url: `${baseOzoneClimatologyURL}/nh_cl/to12.gif`,
+        },
+      ]
+    },
+    normalOzoneMapsSouthern() {
+      const { baseOzoneClimatologyURL } = this
+      return [
+        {
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.JanDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.southern'),
+          url: `${baseOzoneClimatologyURL}/sh_cl/to01.gif`,
+        },
+        {
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.FebDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.southern'),
+          url: `${baseOzoneClimatologyURL}/sh_cl/to02.gif`,
+        },
+        {
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.MarDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.southern'),
+          url: `${baseOzoneClimatologyURL}/sh_cl/to03.gif`,
+        },
+        {
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.AprilDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.southern'),
+          url: `${baseOzoneClimatologyURL}/sh_cl/to04.gif`,
+        },
+        {
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.MayDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.southern'),
+          url: `${baseOzoneClimatologyURL}/sh_cl/to05.gif`,
+        },
+        {
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.JuneDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.southern'),
+          url: `${baseOzoneClimatologyURL}/sh_cl/to06.gif`,
+        },
+        {
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.JulyDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.southern'),
+          url: `${baseOzoneClimatologyURL}/sh_cl/to07.gif`,
+        },
+        {
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.AugustDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.southern'),
+          url: `${baseOzoneClimatologyURL}/sh_cl/to08.gif`,
+        },
+        {
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.SeptDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.southern'),
+          url: `${baseOzoneClimatologyURL}/sh_cl/to09.gif`,
+        },
+        {
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.OctDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.southern'),
+          url: `${baseOzoneClimatologyURL}/sh_cl/to10.gif`,
+        },
+        {
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.NovDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.southern'),
+          url: `${baseOzoneClimatologyURL}/sh_cl/to11.gif`,
+        },
+        {
+          caption:
+            this.$t('data.products.normal-ozone-maps.Images.DecDes') +
+            ' / ' +
+            this.$t('data.products.normal-ozone-maps.southern'),
+          url: `${baseOzoneClimatologyURL}/sh_cl/to12.gif`,
         },
       ]
     },
