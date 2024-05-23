74b61471e8befdf3af9a9922706ff5c4cc8fba72
Mattu <Simran.Mattu@ec.gc.ca>
2024-05-23 12:56:36 -0400

    Added the gallery-carousel to the global tab

--- pages/data/products/normal-ozone-maps.vue
+++ pages/data/products/normal-ozone-maps.vue
@@ -15,12 +15,11 @@
 
     <section v-if="ozoneMapTab === 'global'">
       <h2>{{ $t('data.products.normal-ozone-maps.globalDesc') }}</h2>
-      <v-row>
-        <v-col>
-          <ozone-maps-info />
-        </v-col>
-      </v-row>
-      <v-row>
+      <graph-carousel
+        :key="Object.keys(normalOzoneMapsGlobal)"
+        :graphs="normalOzoneMapsGlobal"
+      ></graph-carousel>
+      <!-- <v-row>
         <v-col>
           <img :src="`${baseOzoneURL}/gl_cl/to01.gif`" class="recent" />
         </v-col>
@@ -67,16 +66,11 @@
         <v-col>
           <img :src="`${baseOzoneURL}/gl_cl/to12.gif`" class="recent" />
         </v-col>
-      </v-row>
+      </v-row> -->
     </section>
 
     <section v-if="ozoneMapTab === 'northern'">
       <h2>{{ $t('data.products.normal-ozone-maps.northernDesc') }}</h2>
-      <v-row>
-        <v-col>
-          <ozone-maps-info />
-        </v-col>
-      </v-row>
       <v-row>
         <v-col>
           <img :src="`${baseOzoneURL}/nh_cl/to01.gif`" class="recent" />
@@ -131,11 +125,6 @@
       <h2>{{ $t('data.products.normal-ozone-maps.southernDesc') }}</h2>
       <v-row>
         <v-col>
-          <ozone-maps-info />
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
           <img :src="`${baseOzoneURL}/sh_cl/to01.gif`" class="recent" />
         </v-col>
         <v-col>
@@ -179,7 +168,10 @@
           <img :src="`${baseOzoneURL}/sh_cl/to11.gif`" class="recent" />
         </v-col>
         <v-col>
-          <img :src="`${baseOzoneURL}/sh_cl/to12.gif`" class="recent" />
+          <img
+            :src="`${baseOzoneClimatologyURL}/sh_cl/to12.gif`"
+            class="recent"
+          />
         </v-col>
       </v-row>
     </section>
@@ -187,55 +179,71 @@
 </template>
 
 <script>
-import OzoneMapsInfo from '~/components/OzoneMapsInfo'
+import GraphCarousel from '~/components/GraphCarousel'
 export default {
   components: {
-    'ozone-maps-info': OzoneMapsInfo,
+    'graph-carousel': GraphCarousel,
   },
   data() {
     return {
-      baseOzoneURL: `https://woudc.org/archive/products/ozone_maps/climatology/`,
+      baseOzoneClimatologyURL: `${this.$config.WOUDC_UI_WAF_URL}/products/ozone_maps/climatology/`,
       ozoneMapTab: 'global',
-      archivedRerender: 0,
-      archivedMeasurement: ['to'],
-      archivedDateRangeType: 'daily', // day / 10 day mean / monthly mean
-      archivedIntervalType: 'day1',
-      archivedHemisphere: 'nh',
-      sourcedForecastHemisphere: ['gl'], // north / south / global
-      sourcedForecastMeasurement: ['to'], // total ozone / std dev.
-      sourcedForecastSubPaths: [],
-      sourcedForecastMapPaths: {},
-      sourcedForecastPathsVerified: false,
-      sourcedForecastLoading: false,
-      sourcedObservedRerender: 0,
-      sourcedObservedHemisphere: ['gl'], // north / south / global
-      sourcedObservedMeasurement: ['to'], // total ozone / std dev.
-      sourcedObservedSubPaths: [],
-      sourcedObservedMapPaths: {},
-      sourcedObservedPathsVerified: false,
-      sourcedObservedLoading: false,
     }
   },
-  head() {
-    return {
-      title: this.$t('data.products.normal-ozone-maps.title'),
-      titleTemplate: this.$titleTemplate(
-        this.$t('common.woudc'),
-        this.$t('common.woudcFull')
-      ),
-      meta: [
+  computed: {
+    normalOzoneMapsGlobal() {
+      const { baseOzoneClimatologyURL } = this
+      return [
         {
-          hid: 'description',
-          name: 'description',
-          content: this.$t('data.products.ozone_maps.description'),
+          caption: '',
+          url: `${baseOzoneClimatologyURL}/gl_cl/to01.gif`,
         },
         {
-          hid: 'keywords',
-          name: 'keywords',
-          content: this.$t('data.products.ozone_maps.keywords'),
+          caption: '',
+          url: `${baseOzoneClimatologyURL}/gl_cl/to02.gif`,
         },
-      ],
-    }
+        {
+          caption: '',
+          url: `${baseOzoneClimatologyURL}/gl_cl/to03.gif`,
+        },
+        {
+          caption: '',
+          url: `${baseOzoneClimatologyURL}/gl_cl/to04.gif`,
+        },
+        {
+          caption: '',
+          url: `${baseOzoneClimatologyURL}/gl_cl/to05.gif`,
+        },
+        {
+          caption: '',
+          url: `${baseOzoneClimatologyURL}/gl_cl/to06.gif`,
+        },
+        {
+          caption: '',
+          url: `${baseOzoneClimatologyURL}/gl_cl/to07.gif`,
+        },
+        {
+          caption: '',
+          url: `${baseOzoneClimatologyURL}/gl_cl/to08.gif`,
+        },
+        {
+          caption: '',
+          url: `${baseOzoneClimatologyURL}/gl_cl/to09.gif`,
+        },
+        {
+          caption: '',
+          url: `${baseOzoneClimatologyURL}/gl_cl/to10.gif`,
+        },
+        {
+          caption: '',
+          url: `${baseOzoneClimatologyURL}/gl_cl/to11.gif`,
+        },
+        {
+          caption: '',
+          url: `${baseOzoneClimatologyURL}/gl_cl/to12.gif`,
+        },
+      ]
+    },
   },
 }
 </script>
